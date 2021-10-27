2764f5ba98840d024999fad63ccd8ca9f7c054d9
Eric Silva <54083779+EricSilvaSt@users.noreply.github.com>
2021-10-27 00:07:35 -0300

    Tabela de Multiplicação + Bônus
    Acrescentada a tabela de multiplicação junto ao bônus da entrega 45.

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,34 @@
-function nomeDaFuncao() { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
+/* Observações da tarefa:
+   let meuArray = [["banana", "maçã"], ["uva", "pêra"]]
+ - banana e maçã = posição 0 do array
+ - uva e pêra = posição 1 do array
+ - banana = posição 0 do array dentro do array
+ - maçã = posição 1 do array dentro do array
+
+    */
+
+function tabelaMultiplicacao(bonus) { 
+
+    let saida = []; // 
+    
+    if (bonus == undefined){
+
+       bonus = 10 
+       // acrescentando o valor 10, sempre que não for declarado um número ao cha,ar uma função
     }
-    console.table(x)
+
+    for (let linhas = 0; linhas <= bonus; linhas++) {
+        
+        saida[linhas] = [];
+       
+        for (let colunas = 0; colunas <= bonus; colunas++){
+            
+            let multiplicacao = linhas * colunas
+            saida[linhas][colunas] = multiplicacao
+        
+        }
+    } 
+    console.table(saida);
+    return saida
 }
+tabelaMultiplicacao();
